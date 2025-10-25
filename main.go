7e1adec199666f51576ec7a575bbdb48530e191f
Gregory Higley <gregory.higley@leapevent.tech>
2025-10-24 23:35:13 -0400

    Finished

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/pelletier/go-toml/v2"
 )
 
 type Config struct {
-	Tools map[string]string
+	Tools map[string]string `json:"tools" toml:"tools"`
 }
 
 func main() {
+	var required = map[string]string{
+		"jq":         "1",
+		"xmlstarlet": "1",
+		"turbopump":  "1",
+	}
 	var path string
 	if len(os.Args) < 2 {
 		path = ".mise.toml"
@@ -20,6 +27,33 @@ func main() {
 	if err != nil && !os.IsNotExist(err) {
 		die(err)
 	}
+	var cfg Config
+	if err == nil {
+		// Exists
+		err = toml.Unmarshal(raw, &cfg)
+		if err != nil {
+			die(err)
+		}
+		for key, value := range required {
+			if _, ok := cfg.Tools[key]; !ok {
+				cfg.Tools[key] = value
+			}
+		}
+	} else if os.IsNotExist(err) {
+		// Not Exists
+		cfg = Config{Tools: required}
+	} else {
+		die(err)
+	}
+	raw, err = toml.Marshal(cfg)
+	if err != nil {
+		die(err)
+	}
+	err = os.WriteFile(path, raw, 0644)
+	if err != nil {
+		die(err)
+	}
+	fmt.Println(cfg)
 }
 
 func die(err error) {
